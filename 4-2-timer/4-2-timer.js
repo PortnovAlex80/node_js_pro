c5d9f53a403d44e567aaf6388de114a86af8f5d6
PortnovAlex80 <savinopoint@ya.ru>
2023-02-17 18:23:33 +0300

    somecorrections

--- 4-2-timer/4-2-timer.js
+++ 4-2-timer/4-2-timer.js
@@ -2,9 +2,7 @@ const isCheck = require('./4-2-timer-check');
 const timingParserToSeconds = require('./4-2-timer-parser');
 const stringTiming = process.argv;
 
-isCheck(stringTiming) && console.log(timingParserToSeconds(stringTiming));
-
-setTimeout(() => {
+isCheck(stringTiming) && setTimeout(() => {
     console.log('Alarm time is run out! Wake up!')
 }, timingParserToSeconds(stringTiming) * 1000);
 
