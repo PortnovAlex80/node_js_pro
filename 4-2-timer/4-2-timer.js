456602869ce9a40aaa77cceb93d9e88405ac5e72
PortnovAlex80 <savinopoint@ya.ru>
2023-02-22 00:42:13 +0300

    timfuns

--- 4-2-timer/4-2-timer.js
+++ 4-2-timer/4-2-timer.js
@@ -1,25 +1,40 @@
+const { EventEmitter} = require('events');
+const { exit } = require('process');
+const { timeInSeconds } = require('./4-2-timer-parser')
+const errorEmitter = new EventEmitter();
+
 const schema = 'hms'.split("");
 const [ , , ...stringTiming] = process.argv;
 const SECONDS_IN_HOUR = 3600;
 const SECONDS_IN_MINUTE = 60;
+const MILLISECONDS_IN_SECONDS= 1000;
 
-// isCheck(stringTiming) && setTimeout(() => {
-//     console.log('Alarm time is run out! Wake up!')
-// }, timingParserToSeconds(stringTiming) * 1000);
+errorEmitter.on('error', (err) => {
+    console.log('ERROR ' + err);
+    exit(-1);
+});
 
-const seconds = stringTiming.filter( args => schema.some(hms => args.slice(-1) === (hms)));
+const timePrefixes = stringTiming.filter( args => schema.some(hms => args.slice(-1) === (hms)));
+(!timePrefixes.length) && errorEmitter.emit('error', `Not present time prefix`);
 
-const set = new Set(seconds);
-set.size !== seconds.length && console.error('Error dublicates');
+const set = new Set(timePrefixes);
+set.size !== timePrefixes.length && errorEmitter.emit('error', `Duplicates found`);
 
-const timer = seconds.map( hms => {
-    const parserValue = Number(hms.split("").slice(0,-1).join(""))
-    if (hms === 'h' && (parserValue < 0 || parserValue > 23)) { 
-         return parserValue * SECONDS_IN_HOUR 
-        };
-    if (hms === 'm' && (parserValue < 0 || parserValue > 23)) { 
-         return parserValue * SECONDS_IN_MINUTE 
-        }; 
-    return ((parserValue))
-})
-console.log(timer)
+// const timeInSeconds = timePrefixes.reduce( (clock, hms) => {  
+//     const value = Number(hms.split("").slice(0,-1).join(""));
+//     const prefix = hms.slice(-1);
+//     if (prefix === 'h') {
+//         return (value > 0 && value  < 23) ? clock += value * SECONDS_IN_HOUR : errorEmitter.emit('error', 'Hours out range 0..23h');
+//         };
+//     if (prefix === 'm') { 
+//         return (value > 0 && value  < 59) ? clock += value * SECONDS_IN_MINUTE : errorEmitter.emit('error', 'Minuts out range 0..59m');
+//         };
+//     return (value > 0 && value  < 59) ? clock += value : errorEmitter.emit('error', 'Seconds out range 0..59m');
+//     }, 0);
+timeInSeconds(timePrefixes);
+
+const milliseconds = timeInSeconds * MILLISECONDS_IN_SECONDS;
+
+setTimeout(() => {
+    console.log('Alarm time is run out! Wake up!');
+}, milliseconds);
