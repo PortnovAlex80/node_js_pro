9e8153b5571821f644fdce84215bed1366aa3911
PortnovAlex80 <savinopoint@ya.ru>
2023-02-22 20:12:25 +0300

    comsetics

--- 4-2-timer/4-2-timer.js
+++ 4-2-timer/4-2-timer.js
@@ -8,29 +8,48 @@ const MILLISECONDS_IN_SECONDS= 1000;
 const SECONDS_IN_HOUR = 3600;
 const SECONDS_IN_MINUTE = 60;
 
-function timeInSeconds(timePrefixes) {
-const seconds = timePrefixes.reduce( (clock, hms) => {  
-    const timePrefix = hms.slice(-1);
-    const value = Number(hms.split("").slice(0,-1).join(""));
-    if (timePrefix === 'h') {
-        return (value > 0 && value  < 23) ? clock += value * SECONDS_IN_HOUR : errorEmitter.emit('error', 'Hours out range 0..23h');
-        };
-    if (timePrefix === 'm') { 
-        return (value > 0 && value  < 59) ? clock += value * SECONDS_IN_MINUTE : errorEmitter.emit('error', 'Minuts out range 0..59m');
-        };
-    return (value > 0 && value  < 59) ? clock += value : errorEmitter.emit('error', 'Seconds out range 0..59m');
-    }, 0);
-return seconds;
-}
-
 errorEmitter.on('error', (err) => {
     console.log('ERROR ' + err);
     exit(-1);
 });
 
-const timePrefixes = stringTiming.filter( args => 
-    schema.some( hms => args.slice(-1) === hms) ? true : errorEmitter.emit('error', `Argument ${args} is not valid`)
-    );
+function isTimeInterval24h(hour) {
+    return (hour > 0 && hour  < 23)
+};
+
+function isTimeInterval60sec(min) {
+    return (min > 0 && min  < 59)
+};
+
+function isTimePrefixOK(timeArgument) {
+    return schema.some( hms => timeArgument.slice(-1) === hms) ? true : errorEmitter.emit('error', `Argument ${timeArgument} is not valid`)
+};
+
+
+function timeInSeconds(timePrefixes) {
+const seconds = timePrefixes.reduce( (clock, hms) => {  
+    const timePrefix = hms.slice(-1);
+    const value = Number(hms.split("").slice(0,-1).join(""));
+    
+    if (timePrefix === 'h') {
+        return isTimeInterval24h(value) 
+        ? clock += value * SECONDS_IN_HOUR 
+        : errorEmitter.emit('error', 'Hours out range 0..23h');
+        };
+    if (timePrefix === 'm') { 
+        return isTimeInterval60sec(value) 
+        ? clock += value * SECONDS_IN_MINUTE 
+        : errorEmitter.emit('error', 'Minuts out range 0..59m');
+        };
+    return isTimeInterval60sec(value) 
+    ? clock += value 
+    : errorEmitter.emit('error', 'Seconds out range 0..59m');
+    }, 0);
+
+return seconds;
+}
+
+const timePrefixes = stringTiming.filter( args => isTimePrefixOK(args));
 
 (!timePrefixes.length) && errorEmitter.emit('error', `No time prefix`);
 
@@ -38,7 +57,6 @@ const set = new Set(timePrefixes);
 set.size !== timePrefixes.length && errorEmitter.emit('error', `Duplicates found`);
 
 const milliseconds = timeInSeconds(timePrefixes) * MILLISECONDS_IN_SECONDS;
-console.log(`Alarm will start by ${milliseconds / MILLISECONDS_IN_SECONDS} seconds`);
 
 setTimeout(() => {
     console.log('Alarm time is run out! Wake up!');
