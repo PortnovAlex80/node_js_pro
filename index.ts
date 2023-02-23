013bc5238f246cbb5b96273069346223eaf706a8
PortnovAlex80 <savinopoint@ya.ru>
2023-02-23 17:45:32 +0300

    add ts

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { userRouter } from './users/users.js'
 
 const port = 8000;
 const app = express();
+
+
 
 app.use((req, res, next) => {
     console.log('Time:', Date.now());
     next();
 });
 
-app.all('/hello', (req, res, next) => {
+app.all('/hello', (req: Request, res: Response, next: NextFunction) => {
         console.log(`Midlware`);
         next();
 });
@@ -19,6 +21,11 @@ app.get('/hello', (req, res) => {
         res.status(201).send('hello')
     }
 );
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log(err.message);
+    res.status(401).send(err.message);
+});
 
 app.use('/users', userRouter)
 
