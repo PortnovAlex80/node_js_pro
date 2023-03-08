f3ebd0813542f8607416001a41f41eefb0b00ee7
PortnovAlex80 <46358607+PortnovAlex80@users.noreply.github.com>
2023-03-08 14:21:46 +0300

    accpet

--- Parser/pas.js
+++ Parser/pas.js
@@ -7,6 +7,43 @@ const acceptEncoding = 'gzip, deflate, br';
 const upgradeInsecureRequests = '1';
 const referer = 'https://www.google.com/';
 
+text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+
+const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
+const accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8';
+const acceptLanguage = 'en-US,en;q=0.9';
+const acceptEncoding = 'gzip, deflate, br';
+const upgradeInsecureRequests = '1';
+const referer = 'https://www.google.com/';
+
+puppeteer.use(StealthPlugin());
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      `--user-agent=${userAgent}`,
+      `--accept=${accept}`,
+      `--accept-language=${acceptLanguage}`,
+      `--accept-encoding=${acceptEncoding}`,
+      `--upgrade-insecure-requests=${upgradeInsecureRequests}`,
+      `--referer=${referer}`
+    ]
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com/');
+
+  // ...
+
+  await browser.close();
+})();
+
 puppeteer.use(StealthPlugin());
 
 (async () => {
