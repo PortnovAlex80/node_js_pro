28a272421bc680444a65308a14a704a65e4df4a8
PortnovAlex80 <savinopoint@ya.ru>
2023-03-08 14:21:09 +0300

    1

--- Parser/posts.js
+++ Parser/posts.js
@@ -7,16 +7,33 @@ const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const MIN_DELAY = 1555;
 const MAX_DELAY = 555;
 
+const userAgent =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
+const acceptLanguage = "en-US,en;q=0.9";
+const acceptEncoding = "gzip, deflate, br";
+const upgradeInsecureRequests = "1";
+const referer = "https://www.bmwclub.com/forums/";
+
 puppeteer.use(StealthPlugin());
 
 (async () => {
   let counter = 17936;
 
   const browser = await puppeteer.launch({
-    headless: true,
-    slowMo: 100,
+    headless: false,
+    slowMo: 500,
     devtools: false,
     executablePath: "/usr/bin/chromium",
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      `--user-agent=${userAgent}`,
+      `--accept=${accept}`,
+      `--accept-language=${acceptLanguage}`,
+      `--accept-encoding=${acceptEncoding}`,
+      `--upgrade-insecure-requests=${upgradeInsecureRequests}`,
+      `--referer=${referer}`,
+    ],
   });
 
   const page = await browser.newPage();
@@ -57,8 +74,8 @@ puppeteer.use(StealthPlugin());
       const delay = MIN_DELAY + Math.random() * MAX_DELAY;
       console.log(`Delay - ${delay}`);
       await page.waitForTimeout(delay);
-      const pause = await new Promise((r) =>
-        setTimeout(r, 250 + Math.random() * 250)
+      await new Promise((r) =>
+        setTimeout(r, 500 + Math.random() * 500)
       );
     }
     // Закрываем выходной файл
