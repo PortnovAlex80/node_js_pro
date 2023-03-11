89deb5f13ce1877264688b1f8aae8973f96df77b
PortnovAlex80 <savinopoint@ya.ru>
2023-03-11 23:57:22 +0300

    added e2e tests

---
+++ api-warehouse/tests/users.e2e-spec.ts
@@ -0,0 +1,58 @@
+import { App } from '../src/app';
+import { boot } from '../src/main';
+import request from 'supertest';
+
+let application: App;
+
+beforeAll(async () => {
+	const { app } = await boot;
+	application = app;
+});
+
+describe('Users e2e', () => {
+	it('Register - error', async () => {
+		const res = await request(application.app).post('/users/register').send({
+			email: 'john5@john.com',
+			login: 'J1',
+			name: 'John',
+			password: 'asdf',
+		});
+		expect(res.statusCode).toBe(409);
+	});
+
+	it('Login - success', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'john5@john.com', password: 'asdf' });
+		console.log(` JWT token -${res.body.Jwt}`);
+		expect(res.body.Jwt).not.toBeUndefined();
+	});
+
+	it('Login - error', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'john5@john.com', password: '1' });
+		expect(res.statusCode).toBe(401);
+	});
+
+	it('Info - success', async () => {
+		const login = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'john5@john.com', password: 'asdf' });
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.Jwt}`);
+		expect(res.body.email).toBe('john5@john.com');
+	});
+
+	it('Info - error', async () => {
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer 1`);
+		expect(res.statusCode).toBe(401);
+	});
+});
+
+afterAll(() => {
+	application.close();
+});
