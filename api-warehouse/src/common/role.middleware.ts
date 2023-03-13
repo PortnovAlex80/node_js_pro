79a22ce114cfc9de7b09bf89e1748bebabcd6364
PortnovAlex80 <savinopoint@ya.ru>
2023-03-13 23:31:44 +0300

    some fi[s

--- api-warehouse/src/common/role.middleware.ts
+++ api-warehouse/src/common/role.middleware.ts
@@ -1,15 +1,10 @@
-import { NextFunction, Response, Request, Router } from 'express';
+import { NextFunction, Response, Request } from 'express';
 import { IMiddleware } from './middleware.interface';
 import { verify } from 'jsonwebtoken';
-import { injectable } from 'inversify';
-import 'reflect-metadata';
-
-@injectable()
 export class RoleMiddleware implements IMiddleware {
 	constructor(private roles: string[], private secret: string) {}
 	execute(req: Request, res: Response, next: NextFunction): void {
 		const token = req.headers.authorization?.split(' ')[1];
-		const path = req.path;
 		if (!token) {
 			res.status(401).send('Not authorized');
 		} else {
