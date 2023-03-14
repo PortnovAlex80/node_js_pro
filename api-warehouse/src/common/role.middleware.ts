58d8a50063dd86a841ac144514a213b4ee7684ee
PortnovAlex80 <46358607+PortnovAlex80@users.noreply.github.com>
2023-03-14 23:19:38 +0300

    Update role.middleware.ts

--- api-warehouse/src/common/role.middleware.ts
+++ api-warehouse/src/common/role.middleware.ts
@@ -24,3 +24,53 @@ export class RoleMiddleware implements IMiddleware {
 		}
 	}
 }
+
+// предложение по улучшению от бота
+import { NextFunction, Response, Request } from 'express';
+import { IMiddleware } from './middleware.interface';
+import { verify, JwtPayload } from 'jsonwebtoken';
+
+const UNAUTHORIZED_STATUS = 401;
+const FORBIDDEN_STATUS = 403;
+const NOT_AUTHORIZED_MESSAGE = 'Not authorized';
+const ACCESS_DENIED_MESSAGE = 'Access denied';
+const INVALID_TOKEN_MESSAGE = 'Invalid token';
+
+export class RoleMiddleware implements IMiddleware {
+  constructor(private roles: string, private secret: string) {}
+
+  execute(req: Request, res: Response, next: NextFunction): void {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+      res.status(UNAUTHORIZED_STATUS).send(NOT_AUTHORIZED_MESSAGE);
+      return;
+    }
+
+    try {
+      const payload = verify(token, this.secret);
+
+      if (isJwtPayload(payload)) {
+        const access: boolean = payload.roles.includes(this.roles);
+        if (!access) {
+          res.status(FORBIDDEN_STATUS).send(ACCESS_DENIED_MESSAGE);
+        } else {
+          next();
+        }
+      } else {
+        res.status(FORBIDDEN_STATUS).send(INVALID_TOKEN_MESSAGE);
+      }
+    } catch (error) {
+      res.status(FORBIDDEN_STATUS).send(INVALID_TOKEN_MESSAGE);
+    }
+  }
+}
+
+function isJwtPayload(payload: unknown): payload is JwtPayload {
+  return (
+    payload !== null &&
+    typeof payload === 'object' &&
+    'roles' in payload &&
+    Array.isArray(payload.roles)
+  );
+}
+
